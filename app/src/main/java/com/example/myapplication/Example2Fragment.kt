5709cae84fe8c2d98799d3648e00b662e12dd85a
arathikv <arathii.kv@gmail.com>
2023-10-13 17:30:58 +0530

    Fragments added

---
+++ app/src/main/java/com/example/myapplication/Example2Fragment.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Example2Fragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_example2, container, false)
+    }
+
+    companion object {
+        @JvmStatic fun newInstance() = Example2Fragment()
+    }
+}
--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -12,5 +13,31 @@ class HomeActivity : AppCompatActivity() {
         val receivedData=intent.getStringExtra("message")
         val welcome=findViewById<TextView>(R.id.welcome)
         welcome.setText("Welcome, $receivedData")
+
+        val frag1=Example1Fragment.newInstance("frag1","frag2")
+        val frag2=Example2Fragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frame1,frag1,"tag_frag1")
+            .commit()
+
+        val button_1=findViewById<Button>(R.id.button_1)
+        val button_2=findViewById<Button>(R.id.button_2)
+
+        button_1.setOnClickListener(){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame1,frag1,"tag_frag1")
+                .commit()
+        }
+        button_2.setOnClickListener(){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame1,frag2,"tag_frag1")
+                .commit()
+        }
+
+
     }
+
+
+
+
 }
